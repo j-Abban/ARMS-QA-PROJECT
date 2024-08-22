bf673dd3c68c7731fde415a347d842319613a6c9
j-Abban <justice.abban@amalitech.org>
2024-08-22 23:37:09 +0000

    Added alure json to pakage.json

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,14 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Add the Allure plugin
+      allureWriter(on, config);
+      
+      // You can implement other node event listeners here
+      return config;
     },
     baseUrl: "https://www.saucedemo.com/",
   },
