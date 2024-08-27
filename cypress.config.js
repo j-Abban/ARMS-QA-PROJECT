cbe7ff6a81fb659ed2b6ff911def6b476f97b95f
j-Abban <justice.abban@amalitech.org>
2024-08-27 09:13:45 +0000

    Allure

--- cypress.config.js
+++ cypress.config.js
@@ -4,12 +4,8 @@ const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // Add the Allure plugin
       allureWriter(on, config);
-      
-      // You can implement other node event listeners here
       return config;
     },
-    baseUrl: "https://www.saucedemo.com/",
   },
 });
